41da4c28318b0a3284dcb05365fc71a4bf4011b2
Tom Douglas <pytd@leeds.ac.uk>
2013-08-30 14:09:49 +0100

    added alma figures and new grid figure

--- makeALMAplot.py
+++ makeALMAplot.py
@@ -34,9 +34,9 @@ def Velarr(ang,V0, A):
     return arr1
 
 def fig11():
-    hdulist1 = pf.open('/home/pytd/documents/SiOJets_New/imagedless1Shock_J2-1_45deg_molcool_dirty.fits')
-    hdulist2 = pf.open('/home/pytd/documents/SiOJets_New/imagedless1Shock_J8-7_45deg_molcool_dirty.fits')
-    hdulist3 = pf.open('/home/pytd/documents/SiOJets_New/imagedless1Shock_J5-4_45deg_molcool_dirty.fits')
+    hdulist1 = pf.open('/import/phy-pc1064_a/Documents/SiOJets_New/run4ALMA/imageShock_J2-1_45deg_molcool_dirty.fits.gz')
+    hdulist2 = pf.open('/import/phy-pc1064_a/Documents/SiOJets_New/run4ALMA/imageShock_J5-4_45deg_molcool_dirty.fits.gz')
+    hdulist3 = pf.open('/import/phy-pc1064_a/Documents/SiOJets_New/run4ALMA/imageShock_J8-7_45deg_molcool_dirty.fits.gz')
     
     
     A = hdulist1[0].header
@@ -48,17 +48,17 @@ def fig11():
     VELarr2 = Velarr(pi/4.,100.0, A)
     print VELarr2[0]
 
-    clevs1 = [0.01,0.03, 0.05]
-    clevs2 = [0.01, 0.1, 0.2, 0.4,0.8,1.2] 
+    clevs1 = [0.0005, 0.001, 0.005, 0.025, 0.125]
+    clevs2 = [0.01,0.05, 0.1, 0.2, 0.4,0.8,1.2] 
     # Get ALMA images
     f1 = plt.figure(figsize=[11,8])
     plt.subplots_adjust(wspace=0.05)
     ax1 = f1.add_subplot(121)
     
-    im1 = imshow(hdulist2[0].data[:,:,:].sum(0),origin='image',vmin = 0.001, vmax = 0.35,cmap=cm.gist_heat)
+    im1 = imshow(hdulist1[0].data[:,:,:].sum(0),origin='image',vmin = 0.001, vmax = 0.25,cmap=cm.gist_heat)
     plt.colorbar(im1,ticks=clevs2)
-    im2 = contour(hdulist3[0].data[:,:,:].sum(0),levels=clevs2,colors='b',linewidths=1.0)
-    im3 = contour(hdulist1[0].data[:,:,:].sum(0),levels=clevs2,colors='g',linewidths=1.0)
+    im2 = contour(hdulist3[0].data[:,:,:].sum(0),levels=clevs2,colors='b',linewidths=.70)
+    im3 = contour(hdulist2[0].data[:,:,:].sum(0),levels=clevs2,colors='g',linewidths=.70)
     
     axis([100.0,250.0,0.0,320.0])
     ax1.xaxis.set_major_locator(MaxNLocator(4))
@@ -69,16 +69,16 @@ def fig11():
     plt.yticks(locs[1:-1],[DECarr2[int(i)] for i in locs[1:-1]],rotation=90)
     ax1.set_ylabel('Declination [J2000]')
     ax1.set_xlabel('Right Ascention [J2000]')
-    plt.figtext(0.16,0.2,r'Image [Jy/beam] : 5-$>$4',color='r')
-    plt.figtext(0.16,0.175,r'Green Contour : 2-$>$1',color='g')
+    plt.figtext(0.16,0.2,r'Image [Jy/beam] : 2-$>$1',color='r')
+    plt.figtext(0.16,0.175,r'Green Contour : 5-$>$4',color='g')
     plt.figtext(0.16,0.15,r'Blue Contour : 8-$>$7',color='b')
     
     ax2 = f1.add_subplot(122)
     
-    im1b = imshow(hdulist2[0].data[:,:,175].T,origin='image',vmin = 0.001, vmax = 0.05,cmap=cm.gist_heat)
+    im1b = imshow(hdulist1[0].data[:,:,175].T,origin='image', vmax = 0.025,cmap=cm.gist_heat)
     plt.colorbar(im1b,ticks=clevs1)
-    im2b = contour(hdulist3[0].data[:,:,175].T,levels=clevs1,colors='b',linewidths=1.0)
-    im3b = contour(hdulist1[0].data[:,:,175].T,levels=clevs1,colors='g',linewidths=1.0)
+    im2b = contour(hdulist3[0].data[:,:,175].T,levels=clevs1,colors='b',linewidths=.70)
+    im3b = contour(hdulist2[0].data[:,:,175].T,levels=clevs1,colors='g',linewidths=.70)
     
     axis([0.0,150.0,0.0,320.0])
     ax2.xaxis.set_major_locator(MaxNLocator(4))
@@ -88,8 +88,8 @@ def fig11():
     plt.setp(ax2,yticks=[])
     ax2.set_xlabel(r'Velocity [km s$^{-1}$]')
     
-    plt.figtext(0.53,0.2,r'PV Diagram [Jy/beam] : 5-$>$4',color='r')
-    plt.figtext(0.53,0.175,r'Green Contour : 2-$>$1',color='g')
+    plt.figtext(0.53,0.2,r'PV Diagram [Jy/beam] : 2-$>$1',color='r')
+    plt.figtext(0.53,0.175,r'Green Contour : 5-$>$4',color='g')
     plt.figtext(0.53,0.15,r'Blue Contour : 8-$>$7',color='b')
     
     plt.show()
